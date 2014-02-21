5375dadb94c5889e5660db71cb4d794fa785316e
eunices <euniceesh.me@gmail.com>
2014-02-21 22:35:30 +0800

    Update 2

---
+++ ggplotAdvise.R
@@ -0,0 +1,6 @@
+# 1) put your data frame in order; 
+# 2) set up a basic plot with the qplot or ggplot function; 
+# 3) add one extra geom at at time (optional if you make a simple plot 
+#                                   with qplot, since qplot sets up the first 
+#                                   geometry for you); 
+# 4) add the settings needed to make the plot look good.
